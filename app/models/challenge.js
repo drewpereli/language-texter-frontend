19b17b04cfeefb8c7ff5c8ccf0a5f4075a1caf2d
Drew Pereli <drewpereli@gmail.com>
2021-08-06 00:28:36 -0700

    Add currentStreak to challenge

--- app/models/challenge.js
+++ app/models/challenge.js
@@ -4,8 +4,8 @@ export default class ChallengeModel extends Model {
   @attr('string') spanishText;
   @attr('string') englishText;
   @attr('number', { defaultValue: 20 }) requiredStreakForCompletion;
+  @attr('number') currentStreak;
   @attr('boolean', { defaultValue: false }) isComplete;
-  @attr('string') createByName;
 
   @belongsTo('user', { async: false }) user;
 }
