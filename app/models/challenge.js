6b46b14dde17ff73a457ca110d723ac3bc9ee59e
Drew Pereli <drewpereli@gmail.com>
2021-09-13 21:22:52 -0700

    Add notes to challenges

--- app/models/challenge.js
+++ app/models/challenge.js
@@ -3,6 +3,8 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class ChallengeModel extends Model {
   @attr('string') spanishText;
   @attr('string') englishText;
+  @attr('string') spanishTextNote;
+  @attr('string') englishTextNote;
   @attr('number', { defaultValue: 20 }) requiredStreakForCompletion;
   @attr('number') currentStreak;
   @attr('string') status;
