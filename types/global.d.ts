b1acd7ca4c4fff61571c6a6259c0eef9b5f9c599
Drew Pereli <drewpereli@gmail.com>
2021-09-20 19:37:53 -0700

    Add eslint for typescript

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,7 @@
 // Types for compiled templates
 declare module 'spanish-texter/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
+  // eslint-disable-next-line prefer-let/prefer-let
   const tmpl: TemplateFactory;
   export default tmpl;
 }
