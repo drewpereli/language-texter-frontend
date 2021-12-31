40803dec0c9fa5ab00490990bd6c93f0d7de6f82
Drew Pereli <drewpereli@gmail.com>
2021-12-31 13:17:52 -0800

    fixup! Add language model

--- app/models/language.ts
+++ app/models/language.ts
@@ -3,5 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class Language extends Model {
   @attr('string') declare code: string;
   @attr('string') declare name: string;
-  @attr('string') declare native_name: string;
+  @attr('string') declare nativeName: string;
 }
