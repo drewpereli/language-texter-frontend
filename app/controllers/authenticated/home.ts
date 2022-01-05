c43981784af5d1b1d7ebd262379b75a0e29ad056
Drew Pereli <drewpereli@gmail.com>
2022-01-04 22:40:38 -0800

    Create new challenges with language set to user's default challenge language

--- app/controllers/authenticated/home.ts
+++ app/controllers/authenticated/home.ts
@@ -4,9 +4,11 @@ import { action } from '@ember/object';
 import StoreService from '@ember-data/store';
 import EmberArray from '@ember/array';
 import ChallengeModel from 'spanish-texter/models/challenge';
+import CurrentUserService from 'spanish-texter/services/current-user';
 
 export default class AuthenticatedHomeController extends Controller {
   @service declare store: StoreService;
+  @service declare currentUser: CurrentUserService;
 
   challengeLists = [
     { status: 'queued', startOpen: false },
@@ -20,6 +22,8 @@ export default class AuthenticatedHomeController extends Controller {
 
   @action
   createNewChallenge(): void {
-    this.store.createRecord('challenge');
+    this.store.createRecord('challenge', {
+      languageId: this.currentUser.user?.userSettings.defaultChallengeLanguage.id,
+    });
   }
 }
